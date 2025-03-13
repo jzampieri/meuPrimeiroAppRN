0edebf906626906f5b4e1e29ab6a0e4c231c805d
Julio Zampieri <juliocezampieri@gmail.com>
2025-03-13 09:31:31 -0300

    style: change app colors

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textinho}>EU SOU SP</Text>
+      <Text style={styles.textinho}>VAI SÂO PAULO</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 200
   },
+  textinho:{
+    color: 'white',
+    fontSize: 30,
+    transform: [{rotate: '90deg'}]
+  }
 });
